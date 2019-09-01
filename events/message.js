722b150ceebb2912656311e80d66e7332c76353b
ZYROUGE <50770114+zyrouge@users.noreply.github.com>
2019-09-01 21:49:23 +0530

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,12 +1,30 @@
+const Enmap = require("enmap");
 module.exports = (bongo, message) => {
-  if (message.author.bot) return;
+  if (message.author.bot) return;  
+  
+  bongo.settings = new Enmap({
+  name: "settings",
+  fetchAll: false,
+  autoFetch: true,
+  cloneLevel: 'deep'
+});
 
-  if (message.content.indexOf(bongo.config.prefix) !== 0) return;
+const defaultSettings = {
+  prefix: "b!"
+}
 
-  const args = message.content.slice(bongo.config.prefix.length).trim().split(/ +/g);
-  const command = args.shift().toLowerCase();
+  const guildConf = bongo.settings.ensure(message.guild.id, defaultSettings);
+  
+  const prefixMention = new RegExp(`^<@!?${bongo.user.id}>( |)$`);
+  if (message.content.match(prefixMention)) {
+    return message.reply(`My prefix on this guild is \`${guildConf.prefix}\``);
+  }
+  
+  if(message.content.indexOf(guildConf.prefix) !== 0) return;
+  const args = message.content.split(/\s+/g);
+  const command = args.shift().slice(guildConf.prefix.length).toLowerCase();
 
-  const cmd = bongo.commands.get(command) || bongo.commands.get(bongo.aliases.get(command));
+  const cmd = bongo.commands.get(command) || bongo.commands.get(bongo.aliases.get(command))
 
   if (!cmd) return;
 
